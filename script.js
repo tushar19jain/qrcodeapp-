0a6f2b266a640b79c451a2c3c0d1a9647a883623
Tushar Jain <112325029+tushar19jain@users.noreply.github.com>
2022-09-30 22:45:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function opennav(){
+    document.getElementById("nav").style.display = "block";
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "block";
+}
+function closenav(){
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("open").style.display = "block";
+    document.getElementById("close").style.display = "none";
+}
